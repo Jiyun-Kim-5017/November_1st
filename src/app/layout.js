52c47a159fdf34ddb40d535e88b9f1b8ebd3a24f
jy5017 <jy5017@giens.co.kr>
2025-08-30 00:19:33 +0900

    meta 정보 수정

--- src/app/layout.js
+++ src/app/layout.js
@@ -6,12 +6,16 @@ export default function RootLayout({children}) {
 
     return (<html>
     <head>
+        <meta charSet="UTF-8"/>
         <meta name="viewport" content="width=device-width, initial-scale=1, user-scalable=no"/>
+        <link rel="icon" type="image/svg+xml" href="/favicon.ico"/>
         <Script
             src="https://developers.kakao.com/sdk/js/kakao.js"
             strategy="beforeInteractive"
         />
-        <title></title>
+        <title>현준❤️지윤</title>
+        <meta property="og:title" content="현준❤️지윤"/>
+        <meta property="og:image" content="/gallery/2.png"/>
     </head>
     <body>
     <ComingSoonTimer/>
